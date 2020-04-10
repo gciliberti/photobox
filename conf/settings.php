286870127e0cfad610427e2b79239065e71b2279
giovanni <g.ciliberti@protonmail.com>
2020-04-10 10:30:55 +0200

    fixed public event route and validator

--- conf/settings.php
+++ conf/settings.php
@@ -19,20 +19,20 @@ return [
             'comment'=> Validator::stringType()->length(1,250),
         ],
         'postEventValidator'=>[
-            'name'=>Validator::alpha("é à è ù ô î â ï ë ö ü")->length(1,120),
+            'name'=>Validator::stringType()->length(1,120),
             'date'=>Validator::regex('/^([1-9]|([012][0-9])|(3[01]))\/([0]{0,1}[1-9]|1[012])\/\d\d\d\d (20|21|22|23|[0-1]?\d):[0-5]?\d/'),
             'location'=>Validator::stringType()->length(5,200),
             'is_public'=>Validator::boolType(),
             'description'=>Validator::stringType()->length(5,250),
         ],
         'putEventValidator'=>[
-            'name'=>Validator::alpha("é à è ù ô î â ï ë ö ü")->length(1,120),
+            'name'=>Validator::stringType()->length(1,120),
             'location'=>Validator::stringType()->length(5,200),
             'date'=>Validator::regex('/^([1-9]|([012][0-9])|(3[01]))\/([0]{0,1}[1-9]|1[012])\/\d\d\d\d (20|21|22|23|[0-1]?\d):[0-5]?\d/'),
             'description'=>Validator::stringType()->length(5,250),
         ],
         'postJoinPrivateEvent'=>[
-            'eventpass'=>Validator::alpha()->length(8,8),
+            'eventpass'=>Validator::alnum()->length(8,8),
         ],
         'playerAuth'=>[
             'pass'=>Validator::alnum()->length(8,8),
--- control/EventController.php
+++ control/EventController.php
@@ -28,13 +28,13 @@ class EventController
             "is_public" => $input['is_public'],
             "description" => $input['description'],
             "members" => array($user['id']),
-            "status"=> "prochainement",
-            "playerpass"=> $playerpass,
+            "status" => "prochainement",
+            "playerpass" => $playerpass,
             "token" => Writer::generateToken(),
         ];
 
-        if($event['is_public']==false){
-            $event['eventpass']=$playerpass = bin2hex(random_bytes(4));
+        if ($event['is_public'] == false) {
+            $event['eventpass'] = $playerpass = bin2hex(random_bytes(4));
         }
 
         $insert = $this->db->event->insertOne($event);
@@ -48,13 +48,13 @@ class EventController
     public function deleteEvent(Request $request, Response $response)
     {
         $user = $request->getAttribute('token');
-        $userId= $user['id'];
+        $userId = $user['id'];
         $eventToken = $request->getAttribute('eventToken');
-        $isOwner = $this->db->event->findOne(['author' => $userId,'token' => $eventToken]);
-        if(isset($isOwner)){// si l'user est bien le proprietaire on supprime l'event
-            $event = $this->db->event->deleteOne(['token'=>$eventToken]);
+        $isOwner = $this->db->event->findOne(['author' => $userId, 'token' => $eventToken]);
+        if (isset($isOwner)) {// si l'user est bien le proprietaire on supprime l'event
+            $event = $this->db->event->deleteOne(['token' => $eventToken]);
             $response = Writer::jsonResponse($response, 204, $event);
-        }else{
+        } else {
             $resp = [
                 "type" => "erreur",
                 "error" => 401,
@@ -69,21 +69,21 @@ class EventController
     public function updateEvent(Request $request, Response $response)
     {
         $input = $request->getParsedBody();
-        if(isset($input['name']) && isset($input['date']) && isset($input['location']) && isset($input['description'])){
+        if (isset($input['name']) && isset($input['date']) && isset($input['location']) && isset($input['description'])) {
             $user = $request->getAttribute('token');
-            $userId= $user['id'];
+            $userId = $user['id'];
             $eventToken = $request->getAttribute('eventToken');
-            $isOwner = $this->db->event->findOne(['author' => $userId,'token' => $eventToken]);
-            if(isset($isOwner)){// si l'user est bien le proprietaire on modifie l'event
+            $isOwner = $this->db->event->findOne(['author' => $userId, 'token' => $eventToken]);
+            if (isset($isOwner)) {// si l'user est bien le proprietaire on modifie l'event
                 $event = [
                     "name" => $input['name'],
                     "date" => $input['date'],
                     "location" => $input['location'],
                     "description" => $input['description'],
                 ];
-                $this->db->event->updateOne(["author" => $userId,"token" => $eventToken],['$set'=> $event]);
+                $this->db->event->updateOne(["author" => $userId, "token" => $eventToken], ['$set' => $event]);
 
-                $UpdatedEvent = $this->db->event->findOne(["author" => $userId,"token" => $eventToken]);
+                $UpdatedEvent = $this->db->event->findOne(["author" => $userId, "token" => $eventToken]);
                 $eventarray = [
                     "id" => (string)$UpdatedEvent->_id,
                     "name" => $UpdatedEvent->name,
@@ -94,7 +94,7 @@ class EventController
                     "token" => $UpdatedEvent->token,
                 ];
                 $response = Writer::jsonResponse($response, 201, $eventarray);
-            }else{
+            } else {
                 $resp = [
                     "type" => "erreur",
                     "error" => 401,
@@ -103,7 +103,7 @@ class EventController
                 $response = Writer::jsonResponse($response, 401, $resp);
 
             }
-        }else{
+        } else {
             $resp = [
                 "type" => "erreur",
                 "error" => 400,
@@ -137,7 +137,14 @@ class EventController
 
     public function getPublicEvents(Request $request, Response $response, $args)
     {
-        $events = $this->db->event->find(['is_public' => true]);
+        $events = $this->db->event->find(['$and' => [
+            ['is_public' => true],
+            ['$or' => [
+                ['status' => "prochainement"],
+                ['status' => "en_cours"]
+            ]]
+        ]
+        ]);
         $evenements = array();
         foreach ($events as $event) {
             $arrayevent = array();
@@ -178,42 +185,43 @@ class EventController
         return $response;
     }
 
-    public function joinPublicEvent(Request $request, Response $response, $args){
+    public function joinPublicEvent(Request $request, Response $response, $args)
+    {
         $user = $request->getAttribute('token');
         $eventtoken = $args["eventtoken"];
         $id = $user['id'];
 
-        if($event = $this->db->event->find(['token' => $eventtoken]))
-        {
+        if ($event = $this->db->event->find(['token' => $eventtoken])) {
             $this->db->event->updateOne(
-                ["token"=>$eventtoken],
-                ['$push'=>['members'=>$id]]
+                ["token" => $eventtoken],
+                ['$push' => ['members' => $id]]
             );
-            $response = Writer::jsonResponse($response, 200, ["success"=>"Join with success"]);
+            $response = Writer::jsonResponse($response, 200, ["success" => "Join with success"]);
             return $response;
         }
-        return $response = Writer::jsonResponse($response, 500, ["error"=>"internal error"]);
+        return $response = Writer::jsonResponse($response, 500, ["error" => "internal error"]);
 
     }
 
-    public function joinPrivateEvent(Request $request, Response $response){
+    public function joinPrivateEvent(Request $request, Response $response)
+    {
         $input = $request->getParsedBody();
         $user = $request->getAttribute('token');
         $id = $user['id'];
 
-        if($event = $this->db->event->find(['eventpass' => $input['eventpass']]))
-        {
+        if ($event = $this->db->event->find(['eventpass' => $input['eventpass']])) {
             $this->db->event->updateOne(
-                ["eventpass"=>$input['eventpass']],
-                ['$push'=>['members'=>$id]]
+                ["eventpass" => $input['eventpass']],
+                ['$push' => ['members' => $id]]
             );
-            $response = Writer::jsonResponse($response, 200, ["success"=>"Join with success"]);
+            $response = Writer::jsonResponse($response, 200, ["success" => "Join with success"]);
             return $response;
         }
-        return $response = Writer::jsonResponse($response, 500, ["error"=>"internal error"]);
+        return $response = Writer::jsonResponse($response, 500, ["error" => "internal error"]);
     }
 
-    public function getEventCreated(Request $request, Response $response, $args){
+    public function getEventCreated(Request $request, Response $response, $args)
+    {
         $user = $request->getAttribute('token');
         $events = $this->db->event->find(['author' => $user['id']]);
         $evenements = array();
@@ -234,9 +242,10 @@ class EventController
         return $response;
     }
 
-    public function getHistory(Request $request, Response $response, $args){
+    public function getHistory(Request $request, Response $response, $args)
+    {
         $user = $request->getAttribute('token');
-        $events = $this->db->event->find(['status' => "fini","members"=>$user['id']]);
+        $events = $this->db->event->find(['status' => "fini", "members" => $user['id']]);
         $evenements = array();
         foreach ($events as $event) {
             $arrayevent = array();
